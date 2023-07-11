432cbaa143a92c8769ad8dd96abb3b89dbe6c6ec
biacoss <biancacostac@outlook.com.br>
2023-07-11 18:35:33 -0300

    Footer finalizado.

---
+++ src/Components/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useLocation, Link } from 'react-router-dom';
+import drinkIcon from '../images/drinkIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
+import '../style/Footer.css';
+
+function Footer() {
+  const location = useLocation();
+  const allowedRoutes = ['/meals', '/drinks', '/profile'];
+  const shouldDisplayFooter = allowedRoutes.includes(location.pathname);
+  if (!shouldDisplayFooter) {
+    return null;
+  }
+
+  return (
+    <footer data-testid="footer" style={ { position: 'fixed', bottom: 0 } }>
+      <Link to="/drinks" data-testid="drinks-bottom-btn" src={ drinkIcon }>
+        <img src={ drinkIcon } alt="Ícone de Bebidas" />
+      </Link>
+      <Link to="/meals" data-testid="meals-bottom-btn" src={ mealIcon }>
+        <img src={ mealIcon } alt="Ícone de Comidas" />
+      </Link>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,8 +1,12 @@
+import Footer from '../Components/Footer';
 import Header from '../Components/Header';
 
 function Drinks() {
   return (
-    <Header />
+    <div>
+      <Header />
+      <Footer />
+    </div>
   );
 }
 
