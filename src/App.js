77ed67c4e53b35bf5ba931e1e8332f697f60df75
Robert Souza G <souzarobert437@gmail.com>
2023-07-12 15:20:45 -0300

    Transferencia de funções e estados para o App.js, main atualizada

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useState, useMemo, useCallback } from 'react';
+import React, { useState, useMemo, useCallback, useEffect } from 'react';
 import './App.css';
-import { Route } from 'react-router-dom';
+import { Route, useHistory, useLocation } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './Components/Login';
 import LoginContext from './Components/Context/Logincontext';
@@ -9,11 +9,63 @@ import Drinks from './pages/Drinks';
 import Profile from './pages/Profile';
 import DoneRecipes from './pages/DoneRecipes';
 import FavoriteRecipes from './pages/FavoriteRecipes';
+import { requestDrinkByFirstLetter, requestDrinkByIngredient,
+  requestDrinkByName, requestMealByFirstLetter, requestMealByIngredient,
+  requestMealByName } from './service/RequestAPI';
 
 function App() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [buttonDisabled, setButtonDisabled] = useState(true);
+  const history = useHistory();
+  const [searchRadio, setSearchRadio] = useState('');
+  const [searchInput, setSearchInput] = useState('');
+  const [requestMeal, setRequestMeal] = useState([]);
+  const [requestDrink, setRequestDrink] = useState([]);
+
+  const requestMealFunctions = {
+    ingredient: requestMealByIngredient,
+    name: requestMealByName,
+    firstLetter: requestMealByFirstLetter,
+  };
+  const requestDrinksFunctions = {
+    ingredient: requestDrinkByIngredient,
+    name: requestDrinkByName,
+    firstLetter: requestDrinkByFirstLetter,
+  };
+
+  const location = useLocation();
+  const handleRadio = ({ target }) => {
+    setSearchRadio(target.value);
+  };
+
+  useEffect(() => {
+    if (requestMeal.length === 1) {
+      history.push(`/meals/${requestMeal[0].idMeal}`);
+    }
+    if (requestDrink.length === 1) {
+      history.push(`/drinks/${requestDrink[0].idDrink}`);
+    }
+  }, [requestDrink, requestMeal, history]);
+
+  const handleBtnBuscar = () => {
+    if (searchRadio === 'firstLetter' && searchInput.length > 1) {
+      global.alert('Your search must have only 1 (one) character');
+      return;
+    }
+    if (location.pathname === '/meals') {
+      requestMealFunctions[searchRadio](searchInput)
+        .then((r) => {
+          setRequestMeal(r.meals);
+        });
+    }
+    if (location.pathname === '/drinks') {
+      requestDrinksFunctions[searchRadio](searchInput)
+        .then((r) => {
+          setRequestDrink(r.drinks);
+        });
+    }
+  };
 
   const buttonStatus = useCallback(() => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -39,9 +91,16 @@ function App() {
       handleChange,
       buttonDisabled,
       buttonStatus,
+      handleBtnBuscar,
+      searchRadio,
+      searchInput,
+      setSearchInput,
+      setSearchRadio,
+      handleRadio,
     }),
     [email, password, handleChange,
-      buttonDisabled, buttonStatus],
+      buttonDisabled, buttonStatus, handleBtnBuscar,
+      searchRadio, searchInput, setSearchInput, setSearchRadio],
   );
 
   return (
